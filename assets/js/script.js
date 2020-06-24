47947a8fb98eb53a3caf53b48d2c8ab6c2f96ff6
binta embalo <bineta1994@gmail.com>
2020-06-24 03:16:52 +0000

    added the testimonial section. added css to the buttons by applying bootstrap "light" classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,3 @@
-//function backgroundColor() {
-  document.getElementById(body).style.background-color= "red";
-//};
-
-$("#read-more").click(function(){
-  $(".more-services").toggle();
-});
-
-
-
 function whatisyourname() {
   let name;
   let person= prompt ("Welcome! Please enter your name.", "name");
@@ -15,10 +5,20 @@ function whatisyourname() {
      prompt ("Welcome! Please enter your name", "name");
   }
   else {
-     enteredName= "Welcome! "  + person
+     enteredName= "welcome! "  + person
   }
   document.getElementById("enter-name").innerHTML= enteredName;
 };
 
 whatisyourname();
+
+function backgroundColor() {
+  document.getElementById("body").style.background= "red";
+};
+
+$(".more-services").hide();
+
+$("#read-more").click(function(){
+  $(".more-services").toggle();
+});
 
